1420dcda72fe620b891898cd9b9c4c9d8a7c6cf2
C1PL3R <138384205+C1PL3R@users.noreply.github.com>
2024-12-14 15:01:25 +0200

    add many many changes

--- ninjago_cinema/ninjago_cinema/settings.py
+++ ninjago_cinema/ninjago_cinema/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from .sercet import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ml3%xuy5qgd_x#6rltwn*k=o2juht2m=yts8lgfjm$b1gh+&+y'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
